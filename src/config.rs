84bb4b8cad0192cd80a38f1d3f1eb2e80d525077
abskiller <xashomon@outlook.com>
2025-05-30 17:31:47 +0800

    修改中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["120.77.76.18"];
+pub const RS_PUB_KEY: &str = "bEvdTz698NbWwZ3i7jPb7cnhofy0sO5BPsyyW3pwNP4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
